f7aff1ed47437704e2d34f610008b46c7deee431
sungjaeahn-kopo <dtw421@naver.com>
2023-01-16 17:21:17 +0900

    Update VideoDetail

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,23 +1,59 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Link, useParams } from "react-router-dom";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import Loader from "./Loader";
+import Videos from "./Videos";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const VideoDetail = () => {
   const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
   const [videos, setVideos] = useState(null);
+  // const [videoDetail, setVideoDetail] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(
       `videos?part=snippet&id=${id}&regionCode=KR&maxResults=25&key=${API_KEY}`
-    ).then((data) => setVideos(data.items));
-  });
+    ).then((data) => setVideos(data.items[0]));
+  }, [id]);
+
+  if (!videos?.snippet) return <Loader />;
+
+  console.log("videos :::1::: ", videos);
+  console.log("videos :::1::: ", videos.snippet);
+  console.log("videos :::1::: ", videos.snippet.title);
+  // 특정 영상정보 선언
+  const {
+    snippet: { title, channelId, channelTitle },
+  } = videos;
 
   return (
     <Box>
       <Stack>
-        <div>${videos}</div>
+        <Box>
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${id}`}
+            className="react-player"
+            controls
+          />
+          <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+            {title}
+          </Typography>
+          <Stack>
+            <Link to={`channel/${channelId}`}>
+              <Typography>
+                {channelTitle}
+                <CheckCircleIcon />
+              </Typography>
+            </Link>
+          </Stack>
+        </Box>
+        <Box>
+          <Videos videos={videos} direction="column" />
+        </Box>
+        {/* <div>${videos}</div> */}
       </Stack>
     </Box>
   );
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,12 @@
 import { Box, Stack } from "@mui/material";
 import React from "react";
 import ChannelCard from "./ChannelCard";
+import Loader from "./Loader";
 import VideoCard from "./VideoCard";
 
 const Videos = ({ videos, direction }) => {
   console.log("videos ::: ", videos);
+  if (!videos?.length) return <Loader />;
   return (
     <Stack
       direction={direction || "row"}
