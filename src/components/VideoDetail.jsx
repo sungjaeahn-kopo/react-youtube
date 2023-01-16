af505deb2c7f26b1eb724e168a2a7bbb16a53f05
sungjaeahn-kopo <dtw421@naver.com>
2023-01-16 18:15:31 +0900

    Update videoDetail UI

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -10,10 +10,14 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 const VideoDetail = () => {
   const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
   const [videos, setVideos] = useState(null);
-  // const [videoDetail, setVideoDetail] = useState(null);
+  const [videoDetail, setVideoDetail] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
+    fetchFromAPI(
+      `videos?part=statistics&id=${id}&regionCode=KR&maxResults=25&key=${API_KEY}`
+    ).then((data) => setVideoDetail(data.items[0]));
+
     fetchFromAPI(
       `videos?part=snippet&id=${id}&regionCode=KR&maxResults=25&key=${API_KEY}`
     ).then((data) => setVideos(data.items[0]));
@@ -21,6 +25,7 @@ const VideoDetail = () => {
 
   if (!videos?.snippet) return <Loader />;
 
+  console.log("videos :::new::: ", videos.statistics);
   console.log("videos :::1::: ", videos);
   console.log("videos :::1::: ", videos.snippet);
   console.log("videos :::1::: ", videos.snippet.title);
@@ -29,31 +34,60 @@ const VideoDetail = () => {
     snippet: { title, channelId, channelTitle },
   } = videos;
 
+  const {
+    statistics: { viewCount, favoriteCount },
+  } = videoDetail;
+
   return (
-    <Box>
-      <Stack>
-        <Box>
-          <ReactPlayer
-            url={`https://www.youtube.com/watch?v=${id}`}
-            className="react-player"
-            controls
-          />
-          <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
-            {title}
-          </Typography>
-          <Stack>
-            <Link to={`channel/${channelId}`}>
-              <Typography>
-                {channelTitle}
-                <CheckCircleIcon />
-              </Typography>
-            </Link>
-          </Stack>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: "#fff" }}
+              py={1}
+              px={2}
+            >
+              <Link to={`channel/${channelId}`}>
+                <Typography
+                  variant={{ sm: "subtitle1", md: "h6" }}
+                  color="#fff"
+                >
+                  {channelTitle}
+                  <CheckCircleIcon
+                    sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+                  />
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(favoriteCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
         </Box>
-        <Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
           <Videos videos={videos} direction="column" />
         </Box>
-        {/* <div>${videos}</div> */}
       </Stack>
     </Box>
   );
